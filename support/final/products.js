8fb61e322dec10e83784c6e072ed361aac88571c
ajith2401 <104023363+ajith2401@users.noreply.github.com>
2022-04-19 09:57:05 -0700

    Add files via upload

---
+++ support/final/products.js
@@ -0,0 +1,153 @@
+const products = [
+  {
+    id: 'rec43w3ipXvP28vog',
+    title: 'Cucumber',
+    company: 'vegitables',
+    image:
+      'https://indiagardening.com/wp-content/uploads/2020/03/3Cucumber.jpg',
+    price: 9.99,
+  },
+  {
+    id: 'rec4f2RIftFCb7aHh',
+    title: 'Pumpkin',
+    company: 'vegitables',
+    image:
+      'https://indiagardening.com/wp-content/uploads/2020/03/4Pumpkin.jpg',
+    price: 79.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'Beetroot',
+    company: 'vegitables',
+    image:
+      'https://indiagardening.com/wp-content/uploads/2020/03/5Beetroot.jpg',
+    price: 25.99,
+  },
+  {
+    id: 'recBohCqQsot4Q4II',
+    title: 'apple',
+    company: 'fruits',
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHOMl_94cFvN1JX81i_R8Q1o9AmNjkXxElBg&usqp=CAU',
+    price: 45.99,
+  },
+  {
+    id: 'recDG1JRZnbpRHpoy',
+    title: 'Bottle-Gourd',
+    company: 'vegitables',
+    image:
+      'https://indiagardening.com/wp-content/uploads/2020/03/1Bottle-Gourd.jpg',
+    price: 6.99,
+  },
+  {
+    id: 'recNWGyP7kjFhSqw3',
+    title: 'Bitter-Gourd',
+    company: 'vegitables',
+    image:
+      'https://indiagardening.com/wp-content/uploads/2020/03/2Bitter-Gourd.jpg',
+    price: 69.99,
+  },
+  {
+    id: 'recZEougL5bbY4AEx',
+    title: 'chicken briyani',
+    company: 'food',
+    image:
+      'https://www.awesomecuisine.com/wp-content/uploads/2007/10/Chicken-Biryani_resized-500x436.jpg',
+    price: 8.99,
+  },
+  {
+    id: 'recjMK1jgTb2ld7sv',
+    title: 'water melon',
+    company: 'fruits',
+    image:
+      'https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/watermelon-sliced-on-wooden-background-1024x575.jpg?w=1155&h=1528',
+    price: 21.99,
+  },
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    title: 'mutton',
+    company: 'meat',
+    image:
+      'https://wallpapercave.com/wp/wp1903756.jpg',
+    price: 39.95,
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    title: 'chicken',
+    company: 'meat',
+    image:
+      'https://media.istockphoto.com/photos/raw-chicken-meat-picture-id907961332?k=20&m=907961332&s=612x612&w=0&h=d20EOEOJdxwk9emxaSyBe0P1vcXd2q7vFKueiDYPwjI=',
+    price: 29.98,
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    title: 'pomegranate',
+    company: 'fruits',
+    image:
+      'https://images.healthshots.com/healthshots/en/uploads/2021/09/27184641/pomegranate.jpg',
+    price: 10.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    title: 'mango',
+    company: 'fruits',
+    image:
+      'https://www.purina.ca/sites/g/files/auxxlc601/files/styles/social_share_large/public/Purina-Article-Can-Dogs-Eat-Mangoes_500x300.png?itok=o2neEMK9',
+    price: 9.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    title: 'pizza',
+    company: 'food',
+    image:
+      'https://img.onmanorama.com/content/dam/mm/en/food/features/images/2021/10/17/pizza.jpg',
+    price: 9.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    title: 'dosa',
+    company: 'food',
+    image:
+      'https://www.holidify.com/images/cmsuploads/compressed/indian-1768906_1920_20180322173733.jpg',
+    price: 9.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'chilli',
+    company: 'vegitables',
+    image:
+      'https://img1.exportersindia.com/product_images/bc-full/dir_63/1860792/fresh-green-chilli-1527052084-3872362.jpeg',
+    price: 25.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'fish',
+    company: 'meat',
+    image:
+      'https://www.bangalore.meatcircle.com/uploads/1587399215_catla-fish-meatcircle.jpg',
+    price: 25.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'tablets',
+    company: 'medicine',
+    image:
+      'https://images.pexels.com/photos/139398/thermometer-headache-pain-pills-139398.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    price: 25.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'pens',
+    company: 'stationary',
+    image:
+      'https://media.istockphoto.com/photos/education-picture-id1156775743?k=20&m=1156775743&s=612x612&w=0&h=E-K8aGRmInL6Rwa-2lcEym3jLJ8g8KDx5eJ6l0_Mjkw=',
+    price: 25.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'burger',
+    company: 'food',
+    image:
+      'https://wallpaperaccess.com/full/1727430.jpg',
+    price: 25.99,
+  },
+];
